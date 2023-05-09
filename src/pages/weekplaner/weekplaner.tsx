4216444fee999e9367677910ca2e0632a1a86e78
michelle2709 <michellehartl2005.com>
2023-05-09 14:20:21 +0200

    weekplaner

---
+++ src/pages/weekplaner/weekplaner.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "../_app";
+import "tailwindcss/tailwind.css";
+
+function WeekPlaner() {
+    const [recipeName, setRecipeName] = useState("Name");
+
+    function createNewDate() {
+        // Hier den Code für die Erstellung eines neuen Datums einfügen
+    }
+
+    return (
+        <div className=" h-screen background">
+            <h1 className="text-center text-4xl font-bold mt-10 mb-8">Weekplaner</h1>
+
+            {/* Date */}
+
+
+            {/* Food that is served on that day of the week */}
+            <section>
+                <div className="grid-container-weekplan">
+                    <div className="grid-item-weekplan-picture">
+                        {/* image here */}
+                    </div>
+                    <div className="grid-item-weekplan-recipe-name">
+                        <h1 id="textRecipeName" className="weekplaner-recipe-name">
+                            {recipeName}
+                        </h1>
+                    </div>
+                </div>
+            </section>
+
+            {/* Add Button */}
+            <section className="fixed bottom-0 right-0 mb-8 mr-8">
+                <form>
+                    <button
+                        onClick={createNewDate}
+                        className="bg-white border-none text-black px-4 py-4 text-center text-sm rounded-full m-2 add-button"
+                        id="addBtn">
+
+                    </button>
+                </form>
+            </section>
+        </div>
+    );
+}
+
+export default WeekPlaner;

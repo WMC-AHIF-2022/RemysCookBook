45f65332d1c1fb1e3210a996c63dc48a92141cb3
michelle2709 <michellehartl2005.com>
2023-06-21 19:06:27 +0200

    shoppingList.ts but no design

--- RemysCookBook/src/client/pages/shoppinglist/shoppingList.ts
+++ RemysCookBook/src/client/pages/shoppinglist/shoppingList.ts
@@ -1,5 +1,46 @@
-import {fetchRestEndpoint} from "../../utils/client-server.js";
+window.onload = () => {
+    const addButton = document.getElementById("add-button");
+    const modalContainer = document.getElementById("modal-container");
+    const itemInput = document.getElementById("item-input") as HTMLInputElement;
+    const okButton = document.getElementById("ok-button");
+    const todoList = document.getElementById("todo-list");
 
-window.onload = async () => {
-    //TODO
-}
+    if (addButton) {
+        addButton.addEventListener("click", () => {
+            if (modalContainer) {
+                modalContainer.style.display = "block";
+                itemInput?.focus();
+            }
+        });
+    }
+
+    if (okButton && todoList) {
+        okButton.addEventListener("click", () => {
+            const itemName = itemInput?.value.trim();
+
+            if (itemName && itemName !== "") {
+                const listItem = document.createElement("li");
+
+                const itemNameSpan = document.createElement("span");
+                itemNameSpan.textContent = itemName;
+
+                const deleteButton = document.createElement("button");
+                deleteButton.textContent = "Delete";
+                deleteButton.addEventListener("click", () => {
+                    todoList.removeChild(listItem);
+                });
+
+                listItem.appendChild(itemNameSpan);
+                listItem.appendChild(deleteButton);
+
+                todoList.appendChild(listItem);
+
+                itemInput.value = "";
+            }
+
+            if (modalContainer) {
+                modalContainer.style.display = "none";
+            }
+        });
+    }
+};
